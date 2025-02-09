8f44668381874dda2a94d829ecc72f66d3e176ed
mohamedelhefenawy <melhefnawy718@gmail.com>
2025-02-09 11:25:47 +0200

    add social media

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,7 +2,7 @@ import project1_img from '../assets/E-commerce_react.png'
 import project2_img from '../assets/Events.png'
 import project3_img from '../assets/E-commerce java.png'
 import project4_img from '../assets/Crud.png'
-
+import project5_img from '../assets/src/assets/Screenshot 2025-02-09 112227.png'
 
 const mywork_data = [
     {
@@ -28,6 +28,12 @@ const mywork_data = [
         w_name:"Web design",
         w_img:project4_img,
         link:'https://crud-7fno.netlify.app//'
+    },
+    {
+        w_no:5,
+        w_name:"Social Media",
+        w_img:project5_img,
+        link:'https://social-app-7fno.vercel.app/'
     }
 ]
  
